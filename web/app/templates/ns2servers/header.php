56f0d661896107015c3ff1c35d29b58fe36f9249
Fursje <force@power-net.org>
2016-02-17 13:11:51 +0100

    layout change

--- web/app/templates/ns2servers/header.php
+++ web/app/templates/ns2servers/header.php
@@ -62,31 +62,33 @@ $hooks = Hooks::get();
 	?>
 
 	<div class="container">
-		<nav class="navbar navbar-light bg-faded">
-			<button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#navbar-header" aria-controls="navbar-header">
-		  		&#9776;
-			</button>
-			<div class="collapse navbar-toggleable-xs" id="navbar-header">
-		  		<a class="navbar-brand" href="#">NS2</a>
-				<ul class="nav navbar-nav">
-					<li class="nav-item active">
-						<a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="/grafana/dashboard/db/natural-selection-2?theme=light" target="_new">Statistics</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Server Setup</a>
-					</li>
-
-					<li class="nav-item">
-						<a class="nav-link" href="#">About</a>
-					</li>
-		  		</ul>
-		  		<form class="form-inline pull-xs-right">
-					<input id="filter" class="form-control" type="text" placeholder="Filter..">
-		  		</form>
+		<div id="custom-bootstrap-menu" class="navbar navbar-default" role="navigation">
+		    <div class="container-fluid">
+			<div class="navbar-header"><a href="/"><img src="<?php print Url::templatePath(); ?>images/ns2logo.png" height="50px" width="50px"></a>
+			    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-menubuilder"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+			    </button>
 			</div>
-		</nav> 
+			<div class="collapse navbar-collapse navbar-menubuilder">
+			    <ul class="nav navbar-nav navbar-left">
+
+				<li class="dropdown">
+				  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Statistics <span class="caret"></span></a>
+				  <ul class="dropdown-menu">
+				    <li><a href="/grafana/dashboard/db/natural-selection-2?theme=light">Generic</a></li>
+				    <li><a href="/grafana/dashboard/db/natural-selection-2-gametypes#">Game modes</a></li>
+				  </ul>
+				</li>
+
+				<li><a href="/contact">Contact</a>
+				</li>
+			    </ul>
+			      <form class="navbar-form navbar-right" role="search">
+				<div class="form-group">
+				  <input id="filter" type="text" class="form-control" placeholder="Search">
+				</div>
+			      </form>
+			</div>
+		    </div>
+		</div>
 	<!-- /navbar -->
 
